14e0f4942f5eedd017a65060c3bcac0d024017a0
Fazliddin <fazliddindehqonov092@gmail.com>
2021-08-12 10:34:21 +0500

    Blog almost works

---
+++ django_project/blog/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Post
+
+def home(request):
+    return render(request, 'blog/home.html', {'posts':Post.objects.all()})
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'about'})
